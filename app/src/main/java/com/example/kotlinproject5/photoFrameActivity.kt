e2a86b7293eecc02f5c07537d417a81d16e6b4cd
whtjtjddn <54929329+whtjtjddn@users.noreply.github.com>
2022-02-21 18:41:05 +0900

    complete Project without ActivitylifeCycle

---
+++ app/src/main/java/com/example/kotlinproject5/photoFrameActivity.kt
@@ -0,0 +1,55 @@
+package com.example.kotlinproject5
+
+import android.net.Uri
+import android.os.Bundle
+import android.widget.ImageView
+import androidx.appcompat.app.AppCompatActivity
+import kotlin.concurrent.timer
+
+class photoFrameActivity: AppCompatActivity() {
+
+    private val photoList = mutableListOf<Uri>()
+    private var currentPosition = 0
+
+    private val photoImageView: ImageView by lazy{
+        findViewById<ImageView>(R.id.photoImageView)
+    }
+    private val backgroundPhotoImageView: ImageView by lazy{
+        findViewById<ImageView>(R.id.backgroundPhotoImageView)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_photoframe)
+
+        getPhotoUriFromIntent()
+        startTimer()
+    }
+
+    private fun getPhotoUriFromIntent(){
+        val size = intent.getIntExtra("photoListSize",0)
+        for(i in 0..size){
+            intent.getStringExtra("photo$i")?.let{
+                photoList.add(Uri.parse(it))
+            }
+        }
+    }
+
+    private fun startTimer(){
+        timer(period = 5000){
+            runOnUiThread{
+                val current = currentPosition
+                val next = if(photoList.size <= currentPosition +1) 0 else currentPosition +1
+
+                backgroundPhotoImageView.setImageURI(photoList[current])
+                photoImageView.alpha = 0f
+                photoImageView.setImageURI(photoList[next])
+                photoImageView.animate()
+                    .alpha(1.0f)
+                    .setDuration(1000)
+                    .start()
+
+                currentPosition = next
+            }
+        }
+    }
+}
